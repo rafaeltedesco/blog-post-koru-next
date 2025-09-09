a60e8c5cb9b63af15a599a8a32c5810e574061d8
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2025-09-08 21:33:36 -0300

    deploy yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "blog-post-koru-next"; 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
